ec5c0178c21ac1dd286978cf1fb576ffa3cbb526
mxcoogi <mxcoogi@gmail.com>
2026-08-22 21:10:47 +0900

    fix: CI에서 upstage 모듈 때문에 SpringBootTest 컨텍스트 로딩 실패하던 문제 수정

--- src/test/java/com/shhdoc/upstage/pipeline/parse/DocumentParserImplIntegrationTest.java
+++ src/test/java/com/shhdoc/upstage/pipeline/parse/DocumentParserImplIntegrationTest.java
@@ -28,6 +28,7 @@ class DocumentParserImplIntegrationTest {
     @BeforeEach
     void setUp() {
         String apiKey = System.getenv("UPSTAGE_API_KEY");
+        documentParser = new DocumentParserImpl(apiKey, ENDPOINT);
     }
 
     @ParameterizedTest
